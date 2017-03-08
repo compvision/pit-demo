462fd68fd86b0d51edac155fd320af6c1c612444
somthing <fake@fake.com>
2017-03-08 04:46:37 +0000

    added saturation values (doesn't work that well) and fixed the contour display so that on the third button press it shows the live feed of the contour and any contours it finds

--- src/TargetDetector.cpp
+++ src/TargetDetector.cpp
@@ -13,7 +13,7 @@ Target* TargetDetector::processImage(Mat input, int i) {
     iterator =i;
     GaussianBlur(input,input,Size(3,3),31);
     //input = canny(thresholdImage(input,53,58,228,238));
-    input = thresholdImage(input,53,58,228,238);
+    input = thresholdImage(input,53,58,0,10,228,238);
     /*cv::namedWindow("Threshold", cv::WINDOW_NORMAL);
     cvSetWindowProperty("Threshold", CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
     cv::namedWindow("Contours", cv::WINDOW_NORMAL);
@@ -22,7 +22,11 @@ Target* TargetDetector::processImage(Mat input, int i) {
       imshow("CV Demo", input);
     }
     input = canny(input);
-    //imshow("Canny", input);
+//   if(iterator == 3)
+ //  {
+  //     imshow("CV Demo", input);
+  // } 
+
     dilate(input, input, Mat());
 
     std::vector<std::vector<Point> > contours = contour(input);
@@ -32,7 +36,10 @@ Target* TargetDetector::processImage(Mat input, int i) {
 
     std::cout <<"processimg: after filter contours" << std::endl;
 
-
+	if(iterator == 3)
+	{
+		imshow("CV Demo", input);
+	}
 
     std::cout << "processimg: imshowed" << std::endl;
 
@@ -64,12 +71,12 @@ Mat TargetDetector::canny(Mat input) {
 }
 
 
-Mat TargetDetector::thresholdImage(Mat input, int minHue, int maxHue, int minVal, int maxVal) {
+Mat TargetDetector::thresholdImage(Mat input, int minHue, int maxHue, int minSat, int maxSat, int minVal, int maxVal) {
     //defining variables
     Mat threshLow;
     Mat threshHigh;
     Mat hueResult;
-    //Mat satResult;
+    Mat satResult;
     Mat valResult;
 
     //seperating image into HSV (or in this case, really just HV)
@@ -79,7 +86,7 @@ Mat TargetDetector::thresholdImage(Mat input, int minHue, int maxHue, int minVal
     std::vector<cv::Mat> separated(3);
     cv::split(cvted, separated);
     Mat hue = separated.at(0).clone();
-    //Mat sat = separated.at(1).clone();
+    Mat sat = separated.at(1).clone();
     Mat val = separated.at(2).clone();
 
     //Hue
@@ -87,17 +94,17 @@ Mat TargetDetector::thresholdImage(Mat input, int minHue, int maxHue, int minVal
     threshold(hue, threshHigh, maxHue, 255, THRESH_BINARY_INV);
     hueResult = threshLow & threshHigh;
 
-    //Saturation
-    /* threshold(sat, threshLow, minSat, 255, THRESH_BINARY);
+  //  Saturation
+     threshold(sat, threshLow, minSat, 255, THRESH_BINARY);
      threshold(sat, threshHigh, maxSat, 255, THRESH_BINARY_INV);
      satResult = threshLow & threshHigh;
-    */
+    
     //Value
     threshold(val, threshLow, minVal, 255, THRESH_BINARY);
     threshold(val, threshHigh, maxVal, 255, THRESH_BINARY_INV);
     valResult = threshLow & threshHigh;
 
-    Mat combined = hueResult & valResult; //& satResult;
+    Mat combined = hueResult & valResult & satResult;
 
     return combined;
 }
@@ -211,7 +218,7 @@ std::vector<std::vector<Point> > TargetDetector::filterContours(std::vector<std:
             std::cout << "min Val: " << minVal << std::endl;
             Target* tempOne = new Target(gearVector[minI]);
             Target* tempTwo = new Target(gearVector[minK]);
-            if(minVal < 0.5 && abs(tempTwo->getCenter().y - tempOne->getCenter().y) < 40)
+            if(minVal < 0.8 && abs(tempTwo->getCenter().y - tempOne->getCenter().y) < 40)
             {
                 std::vector<std::vector<cv::Point> > returnVector;
 
@@ -228,9 +235,10 @@ std::vector<std::vector<Point> > TargetDetector::filterContours(std::vector<std:
 
           Scalar color(255,0,0);
            cv::drawContours(img, returnVector, -1, color, 10);
-           if(iterator ==3){
-             imshow("CV Demo", img);
-           }
+         // if(iterator ==3)
+	   //{
+         //   imshow("CV Demo", img);
+       //    }
                 std::cout << "found gear: " << std::endl;
                 std::cout << "target one center : " << tempOne->getCenter() << std::endl;
                 std::cout << "target two center : " << tempTwo->getCenter() << std::endl;
@@ -240,7 +248,7 @@ std::vector<std::vector<Point> > TargetDetector::filterContours(std::vector<std:
             }
 
         }
-        else if(iterator ==3){
+       /* else if(iterator ==3){
           cv::Mat back(Size(1000,1000), CV_8UC1, Scalar(255, 255, 255 ));
           std::string dis = "Finding Contours";
           cv::putText(back, dis, cv::Point(75,300),
@@ -248,7 +256,7 @@ std::vector<std::vector<Point> > TargetDetector::filterContours(std::vector<std:
           3);
           imshow("CV Demo", back);
 
-        }
+        }*/
         std::cout << "filtercontour: before return an empty vector "<<  std::endl;
         return std::vector<std::vector<cv::Point> >();
     }
